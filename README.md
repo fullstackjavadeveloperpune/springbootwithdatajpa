b245767fedd5d88f837d826755d6f2d8524dba37
jadhavkirangreatlearning <jadhavkiran.greatlearning@gmail.com>
2025-03-21 11:16:31 +0530

    intial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # springbootwithdatajpa
+Spring Boot | Data JPA 
+Functionality-
+1. SignUp
+2. SignIn
+3. FindById
+4. FindAll
+5. FindByName
+6. FindByDOB
+7. FindByAnyInput
+8. SortByName
+9. SortBySalary
+10. Update
+11. DeleteById
+12. DeleteAll
+Custom Exception- If Email Already Exist then throw RecordAlreadyExistException
+
---
+++ springbootwithdatajpa/src/main/java/com/fullstack/SpringbootwithdatajpaApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwithdatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootwithdatajpaApplication.class, args);
+	}
+
+}
